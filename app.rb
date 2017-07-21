18e938985d1b4137c3645db9d383677d99c7447a
Victor Allen <vamuigua@gmail.com>
2017-07-21 11:22:53 +0300

    Updated app

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-require "sinatra/activerecord"
-require "sinatra"
-require "sinatra/reloader"
-also_reload "lib/**/*.rb"
-require('pg')
-require('pry')
+#used bundler to require all gems
+require("bundler/setup")
+#__FILE__ refers to the current file, and File.dirname(__FILE__) will give the current directory
+Bundler.require(:default)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
 
 ####### INDEX ROUTE ########
 get('/') do
