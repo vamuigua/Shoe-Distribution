a18efd207aba7f3f890fbf345a8095eb40b09122
Victor Allen <vamuigua@gmail.com>
2017-07-25 08:20:03 +0300

    Added vaidation of brand and to capitalize comments

--- lib/brand.rb
+++ lib/brand.rb
@@ -2,7 +2,9 @@ class Brand < ActiveRecord::Base
   has_many :shoes
   has_many :stores, through: :shoes
 
+  #Validates presence of a name
   validates(:name, presence: true)
+  #capitalize the name before save
   before_save(:titlecase_name)
 
   private
