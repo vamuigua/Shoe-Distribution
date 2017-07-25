dc77410b6e5c42354de95fd5ac53b0ac433964f6
Victor Allen <vamuigua@gmail.com>
2017-07-25 08:21:14 +0300

    Added vaidation of brand and to capitalize before saving the name

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,14 @@
 class Shoe < ActiveRecord::Base
   belongs_to :brand
   belongs_to :store
+
+  #validated presence of a name
+  validates(:name, :presence => true)
+  #capitalize the name before saving
+  before_save(:titlecase_name)
+
+  private
+  define_method(:titlecase_name) do
+    self.name=(name().titlecase())
+  end
 end
