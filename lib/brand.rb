ff3c9bdee8044e4407543590dcac9fd5b0c0284e
Victor Allen <vamuigua@gmail.com>
2017-07-25 08:17:45 +0300

    Added vaidation of brand and to capitalize

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,4 +1,12 @@
 class Brand < ActiveRecord::Base
   has_many :shoes
   has_many :stores, through: :shoes
+
+  validates(:name, presence: true)
+  before_save(:titlecase_name)
+
+  private
+  define_method(:titlecase_name) do
+    self.name=(name().titlecase())
+  end
 end
